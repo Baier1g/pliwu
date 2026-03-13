9d797b83e9faac23fc4e6fedf059fe6071f1f566
Baier <knapop@gmail.com>
2026-03-13 17:01:40 +0100

    Preliminary work on intermediate representation

--- frontend/ir.c
+++ frontend/ir.c
@@ -1,5 +1,7 @@
 #include "ir.h"
 
+operation *current_op;
+
 char *op_code_to_string(op_code op) {
     switch (op) {
         case MOV:
@@ -33,4 +35,105 @@ char *op_code_to_string(op_code op) {
         case JL:
             return "jl";
     }
+}
+
+operation *create_op(op_code op, operand *arg1, operand *arg2, char *comment) {
+    operation *oper = malloc(sizeof(operation));
+    oper->op = op;
+    oper->arg1 = arg1;
+    oper->arg2 = arg2;
+    oper->comment = comment;
+    return oper;
+}
+
+frame *create_frame() {
+    return (frame *) malloc(sizeof(frame));
+}
+
+void link_operations(operation *op1, operation *op2) {
+    op1->next = op2;
+    op2->prev = op1;
+}
+
+operand *create_operand(operand_type type, void *content) {
+    operand *op = malloc(sizeof(operand));
+    op->type = type;
+    switch(type) {
+        case TEMP:
+        case CONSTANT:
+            op->constant = (int) content;
+            break;
+        case LABEL:
+            op->dest = (operation *) content;
+    }
+}
+
+void recurse_IR_tree(AST_node *node) {
+    if (!node) {
+        return;
+    }
+
+    switch (node->kind) {
+        case A_MODULE:
+            for (linked_list_node *lln = node->module.module_declarations->head; lln != NULL; lln = lln->next) {
+                recurse_IR_tree((AST_node *) lln->data);
+            }
+            break;
+        case A_FUNC_DEF:
+            // MAKE FUNCTIONS
+            break;
+        case A_VAR_DECL:
+            // MAKE VAR_DECL
+            break;
+        case A_BLOCK_STMT:
+            for (linked_list_node *lln = node->block.stmt_list->head; lln != NULL; lln = lln->next) {
+                recurse_IR_tree((AST_node *) lln->data);
+            }
+            break;
+        case A_IF_STMT:
+            // MAKE IF
+            break;
+        case A_PRINT_STMT:
+            // MAKE PRINT
+            break;
+        case A_EXPR_STMT:
+            // MAKE EXPR_STMT
+            break;
+        case A_RETURN_STMT:
+            // MAKE RETURN
+            break;
+        case A_ASSIGN_EXPR:
+            // MAKE ASSIGN
+            break;
+        case A_LOGICAL_EXPR:
+        case A_RELATIONAL_EXPR:
+        case A_ARITHMETIC_EXPR:
+            // MAKE BINARIES
+            break;
+        case A_UNARY_EXPR:
+            // MAKE UNARY
+            break;
+        case A_PRIMARY_EXPR:
+            // MAKE PRIMARIES
+            break;
+        case A_CALL_EXPR:
+            // MAKE CALL
+            break;
+        case A_PARAMETER_EXPR:
+            // MAKE PARAMETER
+            break;
+        default:
+            printf("ir.c::recurse_IR_tree: unknown AST_node kind");
+            break;
+    }
+    return;
+}
+
+operation *create_IR_tree(AST_node *root) {
+    operation *op = create_op(NULL, NULL, NULL, NULL);
+    current_op = op;
+    for (linked_list_node *lln = root->program.modules->head; lln != NULL; lln = lln->next) {
+        recurse_IR_tree((AST_node *) lln->data);
+    }
+    return op;
 }
--- frontend/ir.h
+++ frontend/ir.h
@@ -8,13 +8,29 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct frame frame;
 typedef struct operation operation;
-typedef enum op_code op_code;
 typedef struct operand operand;
+
+typedef enum op_code op_code;
 typedef enum operand_type operand_type;
 
 char *op_code_to_string(op_code);
-operation create_op(operand *, operand *, char *);
+operation *create_op(op_code, operand *, operand *, char *);
+frame *create_frame();
+
+/*
+ * Sets the second argument as the next property of the first operation argument
+ */
+void link_operations(operation *, operation *);
+
+/*
+ * Creates an operand.
+ * The operand_type defines which member of the union is instantiated.
+ */
+operand *create_operand(operand_type, void *);
+
+operation *create_IR_tree(AST_node *);
 
 /*
  * VERY RUDIMENTARY
@@ -24,6 +40,7 @@ enum op_code {
     LEA,
     PUSH,
     POP,
+    PARAM,
 
     ADD,
     ADC,
@@ -34,8 +51,11 @@ enum op_code {
     CMP,
     XOR,
 
+    LABEL,
     CALL,
     RET,
+    INT,
+
     JNC,
     JMP,
     JNE,
@@ -52,18 +72,35 @@ enum operand_type {
     LABEL,
 };
 
+/*
+ * A struct representing a stack frame i.e. a function
+ */
+struct frame {
+    operation* operations;
+    frame **nested_frames;
+};
+
+/*
+ * A struct representing an instruction
+ */
 struct operation {
+    operation *next, *prev;
     op_code op;
     operand *arg1;
     operand *arg2;
     char* comment;
 };
 
+/*
+ * A struct representing an operand.
+ * Can be either a constant, a temporary or a label
+ */
 struct operand {
     operand_type type;
     union {
         int constant;
         operation *dest;
+        frame *call;
     };
 };
 
