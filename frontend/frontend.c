3504151900db37c58bb488d77aa1afae0e17de94
Rasmus <rasmus.ldj@gmail.com>
2026-05-31 16:06:43 +0200

    fixed non-terminated strings

--- frontend/frontend.c
+++ frontend/frontend.c
@@ -52,8 +52,8 @@ int main(int argc, char* argv[]) {
         goto ret;
     }
 
-    //AST_optimiser_constant_folding(prog);
-    AST_printer(prog);
+    AST_optimiser_constant_folding(prog);
+    //AST_printer(prog);
 
     int *count = calloc(1, sizeof(int));
     printf("Converting to IR:\n");
